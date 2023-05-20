e47e542a4960925b8617751333d0a2a142b57609
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-05-20 21:52:40 +0900

    feat:Userのログアウト後のルートを指定

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,20 +3,28 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+    
+    /**
+     * @var string
+     */
+    protected $user_route  = 'user.login';
+
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request):string | null
     {
+        // ルーティングに応じて未ログイン時のリダイレクト先を振り分ける
         if (! $request->expectsJson()) {
-            return route('login');
+            if (Route::is('user.*')) {
+                return route($this->user_route);
+            }
         }
     }
 }

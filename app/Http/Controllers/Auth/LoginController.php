0f7cc5ed88e941e515f16a52293c724603afb725
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-05-20 15:01:51 +0900

    feat:userはログイン->ダッシュボード-> ログアウト-> ログイン画面に遷移を完成

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * ログアウトした時のリダイレクト先
+     */
+    public function loggedOut(Request $request): RedirectResponse
+    {
+        return redirect(route('login'));
     }
 }

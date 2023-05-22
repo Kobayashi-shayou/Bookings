0f5a2ab7d29c1990c03574d6a91fef18bcfbf2b7
Kobayashi-shayou <ykobayashi@minna-systems.co.jp>
2023-05-22 22:04:31 +0900

    feat:ログイン画面に開発中のボタンを作成

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,69 +4,46 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('admin.login') }}">
+            <x-parts.admin_basic_card_layout>
+                <x-slot name="cardHeader">
+                    <h4 class="my-2">ログイン画面：TORiX管理画面</h4>
+                </x-slot>
+                <x-slot name="cardBody">
+                    <form method="POST" action="{{ route('admin.login') }}"  enctype="multipart/form-data">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-md-8 mb-3 mx-auto">
+                            <label class="" for="email">メールアドレス</label>
+                            @include('components.form.text', ['name' => 'email', 'required' => true])
+                            @include('components.form.error', ['name' => 'email'])
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-md-8 mb-3 mx-auto">
+                            <label class="" for="password">パスワード</label>
+                            @include('components.form.password', ['name' => 'password', 'required' => true])
+                            @include('components.form.error', ['name' => 'password'])
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="col-md-8 mb-3 mx-auto">
+                            @include('components.form.checkbox', ['name' => 'remember', 'data' => ['ログイン状態を保持する']])
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('admin.password.request'))
-                                    <a class="btn btn-link" href="{{ route('admin.password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="text-center my-4">
+                            <button type="submit" class="btn btn-dark">
+                                ログイン
+                            </button>
                         </div>
                     </form>
-                </div>
-            </div>
+                    <div class="text-center">
+                        <a href="{{ route('admin.password.request') }}">パスワードを忘れた方</a>
+                        <br>
+                    </div>
+
+                    @if (app()->environment('local'))
+                        <a href="{{ route('admin_dev_login') }}" class="btn btn-dark">開発中ログイン</a>
+                    @endif
+                </x-slot>
+            </x-parts.admin_basic_card_layout>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,41 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-// Auth::routes();
+// トップページ
+Route::get('/top', [HomeController::class, 'top'])->name('top');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// 開発中ログイン(ユーザー)
+Route::get('user_dev_login', function () {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('user')->login(App\Models\User::first());
+    return to_route('user.home');
+})->name('user_dev_login');
+
+Route::get('user_dev_login_id/{id}', function ($id) {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('user')->login(App\Models\User::find($id));
+    return to_route('user.home');
+})->name('user_dev_login_id');
+
+// 開発中ログイン(管理者)
+Route::get('admin_dev_login', function () {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('admin')->login(App\Models\Admin::first());
+    return to_route('admin.home');
+})->name('admin_dev_login');
+
+Route::get('admin_dev_login_id/{id}', function ($id) {
+    abort_unless(app()->environment('local'), 403);
+    auth()->guard('admin')->login(App\Models\Admin::find($id));
+    return to_route('admin.home');
+})->name('admin_dev_login_id');
+
+// エラー画面テスト
+Route::get('500', function () {
+    \Log::error('500エラーのテスト');
+    abort(500);
+});
+
